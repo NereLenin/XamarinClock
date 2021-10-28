e81e2accd333189e4ba03f36a4235e675b331e43
NereLenin <NereLenin25@yandex.ru>
2021-10-28 11:14:58 +0500

    First commit

---
+++ clock/CustomClockCell.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace clock
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomClockCell : ContentView
+    {
+     
+        public CustomClockCell()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void ShowNumber(char OneNotDecadeNumber)
+        {
+
+            Color On = Color.Red;
+            Color Off = Color.FromHex("#5a5a5a");
+
+            switch(OneNotDecadeNumber)
+            {
+                case '0':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = On;
+                        Segm3.BackgroundColor = Segm3.BorderColor = On;
+                        Segm4.BackgroundColor = Segm4.BorderColor = Off;
+                        Segm5.BackgroundColor = Segm5.BorderColor = On;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = On;
+                    }
+                    break;
+                case '1':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = Off;
+                        Segm3.BackgroundColor = Segm3.BorderColor = On;
+                        Segm4.BackgroundColor = Segm4.BorderColor = Off;
+                        Segm5.BackgroundColor = Segm5.BorderColor = Off;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = Off;
+                    }
+                    break;
+                case '2':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = Off;
+                        Segm3.BackgroundColor = Segm3.BorderColor = On;
+                        Segm4.BackgroundColor = Segm4.BorderColor = On;
+                        Segm5.BackgroundColor = Segm5.BorderColor = On;
+                        Segm6.BackgroundColor = Segm6.BorderColor = Off;
+                        Segm7.BackgroundColor = Segm7.BorderColor = On;
+                    }
+                    break;
+                case '3':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = Off;
+                        Segm3.BackgroundColor = Segm3.BorderColor = On;
+                        Segm4.BackgroundColor = Segm4.BorderColor = On;
+                        Segm5.BackgroundColor = Segm5.BorderColor = Off;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = On;
+                    }
+                    break;
+                case '4':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = Off;
+                        Segm2.BackgroundColor = Segm2.BorderColor = On;
+                        Segm3.BackgroundColor = Segm3.BorderColor = On;
+                        Segm4.BackgroundColor = Segm4.BorderColor = On;
+                        Segm5.BackgroundColor = Segm5.BorderColor = Off;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = Off;
+                    }
+                    break;
+                case '5':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = On;
+                        Segm3.BackgroundColor = Segm3.BorderColor = Off;
+                        Segm4.BackgroundColor = Segm4.BorderColor = On;
+                        Segm5.BackgroundColor = Segm5.BorderColor = Off;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = On;
+                    }
+                    break;
+                case '6':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = On;
+                        Segm3.BackgroundColor = Segm3.BorderColor = Off;
+                        Segm4.BackgroundColor = Segm4.BorderColor = On;
+                        Segm5.BackgroundColor = Segm5.BorderColor = On;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = On;
+                    }
+                    break;
+                case '7':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = Off;
+                        Segm3.BackgroundColor = Segm3.BorderColor = On;
+                        Segm4.BackgroundColor = Segm4.BorderColor = On;
+                        Segm5.BackgroundColor = Segm5.BorderColor = Off;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = Off;
+                    }
+                    break;
+                case '8':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = On;
+                        Segm3.BackgroundColor = Segm3.BorderColor = On;
+                        Segm4.BackgroundColor = Segm4.BorderColor = On;
+                        Segm5.BackgroundColor = Segm5.BorderColor = On;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = On;
+                    }
+                    break;
+                case '9':
+                    {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = On;
+                        Segm3.BackgroundColor = Segm3.BorderColor = On;
+                        Segm4.BackgroundColor = Segm4.BorderColor = On;
+                        Segm5.BackgroundColor = Segm5.BorderColor = Off;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = On;
+                    }
+                    break;
+                default: {
+                        Segm1.BackgroundColor = Segm1.BorderColor = On;
+                        Segm2.BackgroundColor = Segm2.BorderColor = On;
+                        Segm3.BackgroundColor = Segm3.BorderColor = On;
+                        Segm4.BackgroundColor = Segm4.BorderColor = Off;
+                        Segm5.BackgroundColor = Segm5.BorderColor = On;
+                        Segm6.BackgroundColor = Segm6.BorderColor = On;
+                        Segm7.BackgroundColor = Segm7.BorderColor = On;
+                        
+                    }
+                    break;
+
+            }
+
+         
+
+        }
+    }
+}
---
+++ clock/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace clock
+{
+    public partial class MainPage : ContentPage
+    {
+        private static System.Timers.Timer timer = new System.Timers.Timer(500);
+        public MainPage()
+        {
+            InitializeComponent();
+
+            timer.Elapsed += Timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Start();
+
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+            //format 08:42:33
+            String current_time = e.SignalTime.TimeOfDay.ToString();
+
+            FirstHoursDisplay.ShowNumber(current_time[0]);
+            SecondHoursDisplay.ShowNumber(current_time[1]);
+
+            FirstMinDisplay.ShowNumber(current_time[3]);
+            SecondMinDisplay.ShowNumber(current_time[4]);
+
+            FirstSecDisplay.ShowNumber(current_time[6]);
+            SecondSecDisplay.ShowNumber(current_time[7]);
+        }
+
+    }
+}
